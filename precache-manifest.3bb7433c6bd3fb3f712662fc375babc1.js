2f7dccc27cd4030aa28fd9089d8e3a232582ea5c
Farid Hernandez <farid.hdz.64@gmail.com>
2020-07-31 12:42:40 -0600

    Updates

---
+++ precache-manifest.3bb7433c6bd3fb3f712662fc375babc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68b0a3f7f3e40f306da3167c449825b",
+    "url": "/react-basic-todo-app/index.html"
+  },
+  {
+    "revision": "297fc94c7b370adbc677",
+    "url": "/react-basic-todo-app/static/css/main.911f922d.chunk.css"
+  },
+  {
+    "revision": "687ea455a4da25256122",
+    "url": "/react-basic-todo-app/static/js/2.e960a5b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-basic-todo-app/static/js/2.e960a5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297fc94c7b370adbc677",
+    "url": "/react-basic-todo-app/static/js/main.7d637669.chunk.js"
+  },
+  {
+    "revision": "97df5c3c10ec610313b2",
+    "url": "/react-basic-todo-app/static/js/runtime-main.d1c53699.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-basic-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
